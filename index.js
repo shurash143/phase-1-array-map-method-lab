ac3b714a9c22f40fc21b6a12a6285e2cc8f13f3c
shurash143 <shukrigarad32@gmail.com>
2025-08-26 17:16:03 +0300

    array-method-map

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(sentence => {
+    return sentence
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  });
+};
+
